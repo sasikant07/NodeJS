11ddff21fc91cea8c02b9501a63b0df4ffd4bc74
Sasikant Biswal <biswalsashikant@gmail.com>
2023-04-24 00:06:54 +0530

    Added Event Emitter Module

---
+++ Events/pizza-shop/drink-machine.js
@@ -0,0 +1,9 @@
+class DrinkMachine {
+    serveDrink(size) {
+        if (size === "large") {
+            console.log("serving complimentry drink");
+        }
+    }
+}
+
+module.exports = DrinkMachine;
