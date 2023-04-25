31775e9b0d6bb7bed57ce4227a1b90df32642dac
Sasikant Biswal <biswalsashikant@gmail.com>
2023-04-25 20:02:16 +0530

    Sending JSON response

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,10 +1,14 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World");
+    const superHero = {
+        firstName: "Bruce",
+        lastName: "Wayne"
+    }
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(superHero));
 });
 
-server.listen(9000, () => {
-    console.log("Server running on port 5000");
+server.listen(4000, () => {
+    console.log("Server running on port 4000");
 })
